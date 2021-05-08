f3617519c3dc66614eae8a558aa495f752e29702
Lancer Abir <anjan.o24@gmail.com>
2021-05-08 23:32:12 +0600

    dd

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+   return (
+      <div>
+         <h1>Footer</h1>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+            facere dolore soluta consectetur eveniet ullam ratione eius neque
+            distinctio. Explicabo neque non esse cumque nisi fugiat iusto
+            numquam recusandae eius in perferendis facere sint consectetur, illo
+            impedit dicta amet similique repellendus, repellat, corrupti a
+            possimus! Est facilis animi quibusdam voluptas.
+         </p>
+      </div>
+   );
+};
+
+export default Footer;
---
+++ src/components/layouts/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+   return (
+      <div>
+         <h1>Header</h1>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, dolor!
+            Provident officiis beatae fugit et enim illum laboriosam ullam animi
+            eos, alias facilis aliquid modi non dicta doloremque quidem
+            quibusdam, necessitatibus totam, esse nam! Dolorum, obcaecati
+            veritatis, odio quasi illum quos, laudantium delectus cumque
+            repellat corrupti eveniet nostrum reiciendis unde!
+         </p>
+      </div>
+   );
+};
+
+export default Header;
